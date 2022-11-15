14b85da630b8854bc64c625fbfd0017cf57e03da
krasikovdp <krasikovdp@yandex.ru>
2022-11-15 17:59:30 +0300

    Small fix

--- main.py
+++ main.py
@@ -6,10 +6,37 @@ from PyQt5.QtWidgets import QApplication, QMessageBox, QSpinBox, QPushButton
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QTextEdit, QLabel
 
 
-class MyWidget(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(455, 332)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(170, 130, 80, 23))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 455, 20))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Круглешки"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
 
         self.qp = QtGui.QPainter()
 
@@ -20,7 +47,7 @@ class MyWidget(QMainWindow):
         r = random.randint(5, 50)
         x = random.randint(r, self.width() - r)
         y = random.randint(r, self.height() - r)
-        self.qp.setPen(QtGui.QColor(190, 190, 0))
+        self.qp.setPen(QtGui.QColor(random.randint(0, 2**24 - 1)))
         self.qp.setBrush(QtGui.QColor(0, 0, 0, 0))
         self.qp.drawEllipse(x, y, r, r)
         self.qp.end()
